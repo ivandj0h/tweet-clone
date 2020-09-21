b0c0bf9df23cd6bcfae17323f41d1ce310c532ba
ivandi1980 <ivandi.djoh@gmail.com>
2020-09-22 04:58:50 +0700

    change component to CobaVue

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome"/>
+  <!-- <HelloWorld msg="Welcome"/> -->
+  <CobaVue test="ini adalah Component CobaVue"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import CobaVue from './components/CobaVue.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld,
+    CobaVue
   }
 }
 </script>
---
+++ src/components/CobaVue.vue
@@ -0,0 +1,12 @@
+<template>
+    <h3>{{ test }} </h3>
+</template>
+
+<script>
+export default {
+    name: 'CobaVue',
+    props: {
+        test: String
+    }
+}
+</script>
